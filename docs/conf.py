ef6e7aa9c4d0df1a01068ca0bc62e30e1b9b6551
Jim Dowling <jdowling@sics.se>
2017-04-03 23:23:32 +0200

    Move jupyter acknowledgement

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Hops Documentation'
-copyright = u'2017, www.hops.io. Based on jupyter-rtd-template.'
+copyright = u'2017, www.hops.io.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
