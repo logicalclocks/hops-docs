36c1c3e098d44fbdd277bb9f3567f3cc775dc8b2
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-05-23 19:36:38 +0200

    Remove chef from simplesetup script

--- docs/getting_started/simplesetup.sh
+++ docs/getting_started/simplesetup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 INSTALL_DEPENDENCIES=false
 DOWNLOAD_DIR=/tmp
 
@@ -12,10 +13,6 @@ if [ "$INSTALL_DEPENDENCIES" = true ] ; then
   wget -P $DOWNLOAD_DIR https://releases.hashicorp.com/vagrant/2.0.2/vagrant_2.0.2_x86_64.deb
   sudo dpkg -i $DOWNLOAD_DIR/vagrant_2.0.2_x86_64.deb
   
-  echo "Installing chefdk..."
-  wget -P $DOWNLOAD_DIR https://packages.chef.io/files/stable/chefdk/2.4.17/ubuntu/16.04/chefdk_2.4.17-1_amd64.deb
-  sudo dpkg -i $DOWNLOAD_DIR/chefdk_2.4.17-1_amd64.deb
-
   echo "Installing virtualbox..."
   wget -P $DOWNLOAD_DIR https://download.virtualbox.org/virtualbox/5.2.6/virtualbox-5.2_5.2.6-120293~Ubuntu~xenial_amd64.deb
   sudo dpkg -i $DOWNLOAD_DIR/virtualbox-5.2_5.2.6-120293~Ubuntu~xenial_amd64.deb
