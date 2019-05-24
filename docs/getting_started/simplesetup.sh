6b06f716b9a6a438cded6b952b68cc4d9e9d4167
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2019-05-24 09:42:20 +0200

    HOPSWORKS-672 (#72)

--- docs/getting_started/simplesetup.sh
+++ docs/getting_started/simplesetup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 INSTALL_DEPENDENCIES=false
 DOWNLOAD_DIR=/tmp
 
@@ -11,11 +12,6 @@ if [ "$INSTALL_DEPENDENCIES" = true ] ; then
   echo "Installing vagrant..."
   wget -P $DOWNLOAD_DIR https://releases.hashicorp.com/vagrant/2.0.2/vagrant_2.0.2_x86_64.deb
   sudo dpkg -i $DOWNLOAD_DIR/vagrant_2.0.2_x86_64.deb
-
-  echo "Installing chefdk..."
-  wget -P $DOWNLOAD_DIR https://packages.chef.io/files/stable/chefdk/2.4.17/ubuntu/16.04/chefdk_2.4.17-1_amd64.deb
-  sudo dpkg -i $DOWNLOAD_DIR/chefdk_2.4.17-1_amd64.deb
-
   echo "Installing virtualbox..."
   wget -P $DOWNLOAD_DIR https://download.virtualbox.org/virtualbox/5.2.6/virtualbox-5.2_5.2.6-120293~Ubuntu~xenial_amd64.deb
   sudo dpkg -i $DOWNLOAD_DIR/virtualbox-5.2_5.2.6-120293~Ubuntu~xenial_amd64.deb
@@ -42,5 +38,5 @@ rm $DOWNLOAD_DIR/virtualbox-5.2_5.2.6-120293~Ubuntu~xenial_amd64.deb
 
 echo "VM Initialization started. Run \"tail -f karamel-chef/nohup.out\" to track progress."
 echo "Once you see the success message, navigate to 127.0.0.1:$HOPSWORKS_PORT/hopsworks"
-echo "on your host machine with credentials user: admin@kth.se password: admin"
+echo "on your host machine with credentials user: admin@hopsworks.com password: admin"
 
