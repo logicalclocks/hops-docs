8468fc7cf49dee2c98503116bd67bb6c39dd249d
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-07-26 13:42:29 +0200

    update simplesetup

--- docs/getting_started/simplesetup.sh
+++ docs/getting_started/simplesetup.sh
@@ -8,19 +8,19 @@ if [ "$1" == "--install-deps" ] ; then
 fi
 
 if [ "$INSTALL_DEPENDENCIES" = true ] ; then
+  echo "Updating apt"
+  sudo apt-get update
   echo "Installing dependencies"
   echo "Installing virtualbox..."
-  wget -P $DOWNLOAD_DIR https://download.virtualbox.org/virtualbox/6.0.8/virtualbox-6.0_6.0.8-130520~Ubuntu~bionic_amd64.deb
-  sudo dpkg -i $DOWNLOAD_DIR/virtualbox-6.0_6.0.8-130520~Ubuntu~bionic_amd64.deb
+  sudo apt install virtualbox
   echo "Installing vagrant..."
   wget -P $DOWNLOAD_DIR https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.deb
   sudo dpkg -i $DOWNLOAD_DIR/vagrant_2.2.4_x86_64.deb
+  sudo apt-get -f -y install
   echo "Installing chefdk..."
-  wget -P $DOWNLOAD_DIR https://packages.chef.io/files/stable/chefdk/4.0.60/ubuntu/18.04/chefdk_4.0.60-1_amd64.deb
-  sudo dpkg -i $DOWNLOAD_DIR/chefdk_4.0.60-1_amd64.deb
-
-  echo "Completing dependency installation"
-  sudo apt -f -y install
+  wget -P $DOWNLOAD_DIR https://packages.chef.io/files/stable/chefdk/2.3.1/ubuntu/16.04/chefdk_2.3.1-1_amd64.deb
+  sudo dpkg -i $DOWNLOAD_DIR/chefdk_2.3.1-1_amd64.deb
+  sudo apt-get -f -y install
 
   echo "Dependency installation completed successfully"
 fi
@@ -36,8 +36,8 @@ HOPSWORKS_PORT=$(./run.sh ports | grep "8181 ->" | awk '{print $3}')
 
 echo "Removing installers"
 rm $DOWNLOAD_DIR/vagrant_2.2.4_x86_64.deb
-rm $DOWNLOAD_DIR/chefdk_4.0.60-1_amd64.deb
-rm $DOWNLOAD_DIR/virtualbox-6.0_6.0.8-130520~Ubuntu~bionic_amd64.deb
+rm $DOWNLOAD_DIR/chefdk_2.3.1-1_amd64.deb
+#rm $DOWNLOAD_DIR/virtualbox-6.0_6.0.8-130520~Ubuntu~bionic_amd64.deb
 
 echo "VM Initialization started. Run \"tail -f karamel-chef/nohup.out\" to track progress."
 echo "Once you see the success message, navigate to https://127.0.0.1:$HOPSWORKS_PORT/hopsworks"
